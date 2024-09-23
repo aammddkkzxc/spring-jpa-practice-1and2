22435f82b3cd1d3fa06a4248225ddf91fb3ac203
aammddkkzxc <ideal0427@naver.com>
2024-09-23 21:26:59 +0900

    feat: 엔티티 클래스 개발2 - 값 타입은 변경 불가능하도록 설계해야함 - setter는 무분별하게 사용하지 않는 것이 좋음. 변경용 비지니스 메서드를 제공하는 것이 나음. 아니면 어디에서 변경이 일어나서 문제가 생겼는지 추적하기 어려움 - 기본키 칼럼 명을 테이블_id 형식으로 하는 것은 db에서도 어디 소속인지 명확하게 알기 위해서

--- src/main/java/jpabook/jpashop/domain/Address.java
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -1,14 +1,19 @@
 package jpabook.jpashop.domain;
 
 import jakarta.persistence.Embeddable;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 
 @Embeddable
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED, force = true)
+@RequiredArgsConstructor
 public class Address {
 
-    private String city;
-    private String street;
-    private String zipCode;
+    private final String city;
+    private final String street;
+    private final String zipCode;
 
 }
