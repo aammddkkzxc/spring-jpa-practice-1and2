1069fcc1c931a7b8d04db133c7fd03ec8ea62edd
aammddkkzxc <ideal0427@naver.com>
2024-09-23 15:56:19 +0900

    feat: JPA와 DB 설정, 동작확인 - 쿼리와 커맨드 분리 (CQRS패턴)   - 커맨드 작업에서 전체 엔티티를 반환할 경우 그 데이터를 호출하는 쪽에서 수정하거나 변형할 위험이 있다. 이렇게 엔티티를 불필요하게 다루는 과정에서 예상치 못한 사이드 이펙트가 발생할 수 있다. - 인텔리제이 말고 cmd로 빌드 해보는 연습 참고https://dev-coco.tistory.com/68 - 쿼리 파라미터 로그 남기기 -> p6spy 외부 라이브러리 의존성 추가 (개발 단계에서만 쓰는것 권장)

---
+++ src/main/java/jpabook/jpashop/MemberRepository.java
@@ -0,0 +1,23 @@
+package jpabook.jpashop;
+
+import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class MemberRepository {
+
+    private final EntityManager em;
+
+    //엔티티 아닌 id 반환
+    public Long save(Member member) {
+        em.persist(member);
+        return member.getId();
+    }
+
+    public Member find(Long id) {
+        return em.find(Member.class, id);
+    }
+
+}
