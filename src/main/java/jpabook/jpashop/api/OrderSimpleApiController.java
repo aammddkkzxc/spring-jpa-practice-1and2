cb068705a4f24f9d96b8daa887ade9eccdaafc7b
aammddkkzxc <ideal0427@naver.com>
2024-10-04 03:07:56 +0900

    feat: 간단한 주문 조회 V3: 엔티티를 DTO로 변환 - 페치 조인 최적화 - 이 방법은 select쿼리를 딱 원하는 것들만 가져오기 때문에 성능상 더 우수할 수 있으나 직접 하나하나 지정해주었기 때문에 재사용성이 안좋다 - 데이터 필드가 너~무 많은 경우엔 쓰일 수 있을 것 같다 - 그 외 대부분의 경우엔 이 전 방법이 좋아보인다. 성능에 영향을 미치는 다른 요소를 찾아보는게 더 나을듯 (인덱스가 잘못 잡혔다거나) - 이러한 특수한 조회용 쿼리는 레포지토리 분리하고, 따로 패키지를 만들어서 두는 것이 낫다. 왜냐하면 레포지토리의 역할은 원래 순수한 엔티티를 반환하는 것인데, 반면 이 기능은 화면 스펙에 의존하게 되는 현상이 있음. -> 분리를 통해 유지보수성을 꾀한다 - 참고로 jpql new operation 생성자에 엔티티를 보내면 식별자(id)가 들어가기 때문에 직접 하나하나 해줘야했음

--- src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -5,6 +5,8 @@ import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.OrderSearch;
 import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
+import jpabook.jpashop.repository.order.simpleQuery.OrderSimpleQueryDto;
+import jpabook.jpashop.repository.order.simpleQuery.OrderSimpleQueryRepository;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,6 +22,7 @@ import java.util.stream.Collectors;
 public class OrderSimpleApiController {
 
     private final OrderRepository orderRepository;
+    private final OrderSimpleQueryRepository orderSimpleQueryRepository;
 
     @GetMapping("/api/v1/simple-orders")
     public List<Order> getSimpleOrders() {
@@ -47,6 +50,11 @@ public class OrderSimpleApiController {
 
         return orders.stream().map(order -> new SimpleOrderDto(order))
                 .collect(Collectors.toList());
+    }
+
+    @GetMapping("/api/v4/simple-orders")
+    public List<OrderSimpleQueryDto> ordersV4() {
+        return orderSimpleQueryRepository.findOrderDtos();
     }
 
     @Data
